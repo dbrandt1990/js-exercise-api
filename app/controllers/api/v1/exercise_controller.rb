3c18cfb1250f181402f80e64e5e3e498f5082e47
dbrandt1990 <dvbrandt90@gmail.com>
2021-06-10 11:31:57 -0600

    strong params and create action for exercise

--- app/controllers/api/v1/exercise_controller.rb
+++ app/controllers/api/v1/exercise_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::ExerciseController < ApplicationController
         exercises = Exercise.all
         render json: exercises
     end
+
+    def create  
+        exercise = Exercise.new(exercise_params)
+
+        if exercise.save
+            render json: exercise, status: :accepted
+        else
+            render json: {errors: exercise.errors.full_messages}, status: unprocessible_entity
+        end
+
+    end
+
+    private 
+
+    def exercise_params
+        params.require(:exercise).permit(:name, :description, :category)
+    end
 end
