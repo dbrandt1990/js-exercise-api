9dea4567e8b038a7ae06556b350aa8fcc12e3cf2
dbrandt1990 <dvbrandt90@gmail.com>
2021-06-30 13:55:26 -0600

    exercise associations working and routines rendering

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -6,8 +6,16 @@ class Api::V1::RoutinesController < ApplicationController
 
     def create  
         routine = Routine.new(routine_params)
+        exercises =  routine_params["content"].split(",")
 
         if routine.save
+            # add exercise associations
+            exercises.each do |exercise| 
+                ex = Exercise.find_by(name: exercise.split(":").first.strip)
+                if ex
+                    routine.exercises << ex
+                end
+            end
             render json: RoutineSerializer.new(routine), status: :accepted
         else
             render json: {errors: routine.errors.full_messages}, status: unprocessible_entity
@@ -18,6 +26,6 @@ class Api::V1::RoutinesController < ApplicationController
     private 
 
     def routine_params
-        params.require(:routine).permit(:title, :content :exercises)
+        params.require(:routine).permit(:title, :content, :category_id)
     end
 end
